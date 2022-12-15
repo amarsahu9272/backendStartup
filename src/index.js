49d36f95949e9630c96f3372a243894149f4127b
Anil Singh <anil927a@gmail.com>
2022-12-15 20:27:34 +0530

    session/mongo-initial commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
-var bodyParser = require('body-parser');
-
+const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-
 const app = express();
+const mongoose = require('mongoose')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//connecting nodejs app with mongoDb
+mongoose.connect("mongodb+srv://amarsaahu1997:Qwerty123@student-cluster0.hlzlzz3.mongodb.net/test", { useNewUrlParser: true })
+    .then(() => console.log('mongodb running on 27017'))
+    .catch(err => console.log(err))
+
 app.use('/', route);
 
-app.listen(3000, function() {
-    console.log('Express app running on port ' + 3000)
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
-
-
-// A || B
