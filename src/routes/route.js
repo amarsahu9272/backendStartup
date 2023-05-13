fe54eaa84719bf2771a9e91fdd7842afa6d7495d
amar <amarsaahu1997@gmail.com>
2023-05-14 02:05:51 +0530

    assignment/voting-age/query-params

--- src/routes/route.js
+++ src/routes/route.js
@@ -60,4 +60,45 @@ router.post("/postQueryLength", function (req, res) {
   res.send({ data: finalArr, status: true })
 })
 
+//---------------------------assignment-query-params-----------------------------------------------------
+let persons = [
+  {
+    name: "PK",
+    age: 10,
+    votingStatus: false,
+  },
+  {
+    name: "SK",
+    age: 20,
+    votingStatus: false,
+  },
+  {
+    name: "AA",
+    age: 70,
+    votingStatus: false,
+  },
+  {
+    name: "SC",
+    age: 5,
+    votingStatus: false,
+  },
+  {
+    name: "HO",
+    age: 40,
+    votingStatus: false,
+  },
+];
+
+router.post("/query-params", function (req, res) {
+  let input = req.query.input;
+  console.log(typeof input)
+  let arr = [];
+  for (i = 0; i < persons.length; i++) {
+    if (persons[i].age > input) {
+      persons[i].votingStatus = true;
+      arr.push(persons[i]);
+    }
+  }
+  return res.send(arr);
+});
 module.exports = router;
