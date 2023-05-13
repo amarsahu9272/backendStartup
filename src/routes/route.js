80ad81bdf7fadc5bb51a72a979c6d0fb516aa303
amar <amarsaahu1997@gmail.com>
2023-05-14 01:21:40 +0530

    path-params/query-params

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// In JavaScript, path parameters are typically used in the context of routing,
+// where a URL path is parsed and values are extracted from it to determine which page or resource to display.
+
+// Path parameters are parts of the URL path that are variable, 
+// and are often denoted by a colon (:) followed by a parameter name.
+// For example, in the URL /users/:userId/posts/:postId, :userId and :postId are path parameters.
+
+// path-parameters are used to load different items/page based on a variable in the URL
+// and Parameters variable name is not visible in the URL
+router.get('/users/:userId/posts/:postId', (req, res) => {
+  const userId = req.params.userId;
+  const postId = req.params.postId;
+  res.send(`User ID: ${userId}, Post ID: ${postId}`);
+});
+// When a GET request is made to this route, Express automatically extracts 
+// the values of the userId and postId parameters from the URL and stores them in the 
+// req.params object. We can then access these values using req.params.userId and req.params.postId.
+
 router.get('/path-params/:name/:lastname', function (req, res) {
   res.send({
     'name': req.params.name,
@@ -9,6 +27,20 @@ router.get('/path-params/:name/:lastname', function (req, res) {
 });
 
 
+
+
+
+
+
+
+
+// In JavaScript, query parameters are a common way to pass information 
+// from the client-side (e.g., a web browser) to the server-side. 
+// Query parameters are typically appended to the end of a URL and are indicated by a question mark (?) 
+// followed by one or more key-value pairs separated by an ampersand (&).
+
+// query params is used for filters and Searches
+// and query Parameters variable name is visible in the URL itself
 router.get('/query-params', function (req, res) {
 
   res.send({
@@ -16,6 +48,8 @@ router.get('/query-params', function (req, res) {
     "productPrice": req.query.productPrice,
   })
 })
-
+// When a GET request is made to this route, Express automatically extracts 
+// the values of parameters from the URL and stores them in the 
+// req.query object. We can then access these values using req.query.productName and req.query.productPrice.
 
 module.exports = router;
