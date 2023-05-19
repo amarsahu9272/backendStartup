be86faedcd69b3d962e02488a6b9d4d8026e57c0
amar <amarsaahu1997@gmail.com>
2023-05-19 07:13:44 +0530

    changes in conecction/.env

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const dotenv = require("dotenv");
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const app = express();
-const mongoose = require('mongoose')
+// const cors = require("cors");
 
+dotenv.config();
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.set("strictQuery", false);
 //connecting nodejs app with mongoDb
-mongoose.connect("mongodb+srv://amarsaahu1997:Qwerty123@student-cluster0.hlzlzz3.mongodb.net/test", { useNewUrlParser: true })
-    .then(() => console.log('mongodb running on 27017'))
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err))
 
 app.use('/', route);
